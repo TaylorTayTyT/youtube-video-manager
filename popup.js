df003e127a628420ccbf2c939f3c61d564484442
Taylor Nguyen <taylornguyen904@gmail.com>
2025-04-29 13:20:37 -0700

    added some ui logic

--- popup.js
+++ popup.js
@@ -8,16 +8,16 @@ async function gatherVideos() {
     const tabIds = tabs.map(({ id }) => id);
     const videosParentElement = document.getElementById("videos");
     const savedData = await chrome.storage.local.get();
-    console.log(savedData)
-    Object.keys(savedData).forEach(key =>{
-        console.log(key, tabIds)
-        console.log(tabIds.includes(parseInt(key)))
-        if (!tabIds.includes(parseInt(key))){
+    //console.log(savedData)
+    Object.keys(savedData).forEach(key => {
+        //console.log(key, tabIds)
+        //console.log(tabIds.includes(parseInt(key)))
+        if (!tabIds.includes(parseInt(key))) {
             delete savedData[key];
         }
     });
     chrome.storage.local.clear()
-    .then(() => chrome.storage.local.set(savedData));
+        .then(() => chrome.storage.local.set(savedData));
     tabIds.forEach(async (video) => {
         // information for the block
         const v = document.createElement("li");
@@ -38,6 +38,7 @@ async function gatherVideos() {
         playButton.innerText = "play";
         playButton.setAttribute("tabId", vi.id);
         playButton.onclick = playVideo;
+        playButton.classList.add("play");
 
         //pauseButton setup
         const pauseButton = document.createElement("img");
@@ -46,6 +47,7 @@ async function gatherVideos() {
         pauseButton.innerText = "pause";
         pauseButton.onclick = pauseVideo;
         pauseButton.setAttribute("tabId", vi.id);
+        pauseButton.classList.add("pause");
 
         //replay setup
         const replayButton = document.createElement("img");
@@ -62,7 +64,7 @@ async function gatherVideos() {
         navigateButton.onclick = navigate;
 
         //memory
-        if(Object.keys(savedData).includes(String(vi.id))){
+        if (Object.keys(savedData).includes(String(vi.id))) {
             title.innerText = savedData[vi.id];
         }
 
@@ -79,21 +81,21 @@ async function gatherVideos() {
 
 function renameVideo(element) {
     element.target.style.display = 'none';
-    console.log(element)
+    //console.log(element)
     const tabId = element.target.getAttribute("tabId");
     const input = document.querySelector(`input[tabId="${tabId}"]`);
     input.value = element.target.innerText;
-    console.log(`input[tabId=${tabId}]`)
+    //console.log(`input[tabId=${tabId}]`)
     input.style.display = "block";
     async function save(e) {
         if (input !== document.activeElement || e.key && e.key === 'Enter') {
             input.style.display = "none";
             element.target.style.display = "block";
             element.target.innerText = input.value;
-            const title_storage = {}; 
-            document.querySelectorAll(".title").forEach(title =>{
+            const title_storage = {};
+            document.querySelectorAll(".title").forEach(title => {
                 const tabId = title.getAttribute("tabId");
-                title_storage[tabId] = title.innerText; 
+                title_storage[tabId] = title.innerText;
             });
 
             await chrome.storage.local.set(title_storage)
@@ -108,21 +110,42 @@ function renameVideo(element) {
 
 function playVideo(e) {
     const tabId = parseInt(e.target.getAttribute("tabid"));
+    //console.log(e.target)
     chrome.scripting.executeScript({
         target: { tabId: tabId },
         func: () => {
             document.querySelector("video").play();
         }
+    }, () => {
+        e.target.classList.add("selected");
+        console.log(e.target.parentElement);
+        e.target.parentElement.childNodes.forEach((child) => {
+            console.log(child.classList);
+            if (child.classList.contains("pause")) {
+                console.log(child)
+                child.classList.remove("selected");
+            }
+        })
     })
 };
 function pauseVideo(e) {
     const tabId = parseInt(e.target.getAttribute("tabid"));
-    console.log(tabId)
+    //console.log(tabId)
     chrome.scripting.executeScript({
         target: { tabId: tabId },
         func: () => {
             document.querySelector("video").pause();
         }
+    }, () => {
+        e.target.classList.add("selected");
+        console.log(e.target.parentElement);
+        e.target.parentElement.childNodes.forEach((child) => {
+            console.log(child.classList);
+            if (child.classList.contains("play")) {
+                console.log(child)
+                child.classList.remove("selected");
+            }
+        })
     })
 }
 function replay(e) {
@@ -132,11 +155,18 @@ function replay(e) {
         func: () => {
             document.querySelector("video").currentTime = 0;
         }
+    }, () => {
+
+        e.parentElement().childNodes().forEach((child) => {
+            if (child.innerText === "pause") {
+                child.classList.remove("selected");
+            }
+        })
     })
 };
 
-function navigate(e){
+function navigate(e) {
     const tabId = parseInt(e.target.getAttribute("tabid"));
-    chrome.tabs.update(tabId, {active: true});
+    chrome.tabs.update(tabId, { active: true });
 }
 gatherVideos(); 
